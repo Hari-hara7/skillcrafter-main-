11e096268ef3bfda773386756eba585946ce7d20
Your Name <your.email@example.com>
2024-06-17 18:58:35 +0530

    hari

--- script.js
+++ script.js
@@ -52,3 +52,16 @@ function toggleMenu() {
      navbar.style.display = "block";
    }
  }
+
+
+
+ // script.js
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+  var navbar = document.getElementById('navbar');
+  if (navbar.style.display === 'none' || navbar.style.display === '') {
+      navbar.style.display = 'block';
+  } else {
+      navbar.style.display = 'none';
+  }
+});
